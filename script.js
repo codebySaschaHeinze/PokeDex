d9db91bebf2fe5c374b435d7385090e5b350f7e4
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-07-29 18:23:53 +0200

    style: hover on cards and imgs script: experiment with unsortet fetchoutput. html: load_more_button added (no function yet)

--- script.js
+++ script.js
@@ -1,29 +1,58 @@
+const typeColors = {
+  fire: "#ff0000ff",
+  water: "#2092fcff",
+  grass: "#2b8a15ff",
+  electric: "#f8ed1dff",
+  psychic: "#97009fff",
+  normal: "#d75d00ff",
+  bug: "#b47e00ff",
+  ground: "#775300ff",
+  poison: "#8be700ff",
+  fighting: "#002c9dff",
+  rock: "#5e5e5eff",
+  fairy: "#f400f4ff",
+  ice: "#9ff1ffff",
+  dragon: "#3b6300ff",
+  ghost: "#ffffffff",
+  dark: "#4e006fff",
+};
+
 function init() {
   getPokemons();
-  // loadingScreen();
 }
 
 async function getPokemons() {
   let charsRef = document.getElementById("main-content-container");
-  charsRef.innerHTML = "";
+  loadingScreen();
 
   try {
     const url = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
     const response = await fetch(url);
     const chars = await response.json();
-    console.log(chars.results);
-
-    for (let i = 0; i < chars.results.length; i++) {
-      const char = chars.results[i];
-      charsRef.innerHTML += pokemonTemplate(char);
-      // console.log(char.results);
+    const results = chars.results;
+    charsRef.innerHTML = "";
+    for (let i = 0; i < results.length; i++) {
+      getPokemonDetails(results[i]);
     }
   } catch (error) {
     charsRef.innerHTML = errorTemplate();
   }
 }
 
-// function loadingScreen() {
-//   let spinRef = document.getElementById("main-content-container");
-//   spinRef.innerHTML = loadingTemplate();
-// }
+async function getPokemonDetails(char) {
+  try {
+    const details = await fetch(char.url);
+    const data = await details.json();
+    const charsRef = document.getElementById("main-content-container");
+    charsRef.innerHTML += pokemonDetailsTemplate(data);
+  } catch (error) {}
+}
+
+function capitalize(upperCase) {
+  return upperCase.charAt(0).toUpperCase() + upperCase.slice(1);
+}
+
+function loadingScreen() {
+  let spinRef = document.getElementById("main-content-container");
+  spinRef.innerHTML = loadingTemplate();
+}
--- scripts/template.js
+++ scripts/template.js
@@ -1,17 +1,25 @@
-// function loadingTemplate() {
-//   return `
-//   <div class="pokeball_loader">
-//     <img src="./assets/icons/pokeball_spin.png" alt="" />
-//   </div>`;
-// }
-
-function errorTemplate() {
-  return `<p>Seite konnte nicht geladen werden</p>`;
+function loadingTemplate() {
+  return `
+          <div class="pokeball_loader">
+            <img src="./assets/icons/pokeball_spin.png" alt="" />
+          </div>`;
 }
 
-function pokemonTemplate(char) {
-  console.log(char);
+function errorTemplate() {
+  return `
+          <p>Seite konnte nicht geladen werden</p>`;
+}
 
-  return `<p>${char.name}</p>
-          <img src="${char.url}" alt="">`;
+function pokemonDetailsTemplate(pokemon) {
+  const pokemonType = pokemon.types[0].type.name;
+  const color = typeColors[pokemonType];
+  return `
+          <div
+            id="pokemon-card" class="pokemon_card" style="background-color: ${color}">
+              <p>#${pokemon.id}</p>
+              <h5>${capitalize(pokemon.name)}</h5>
+              <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}" />
+              <p class="pokemon_type">${pokemon.types[0].type.name}</p>
+          </div>
+        `;
 }
